7058574b55de1960e9a9f0589d5985183b29f654
Driss ABSIKE <absikedriss25@gmail.com>
2025-11-05 15:23:31 +0100

    first commit

---
+++ src/test/java/com/Renault/MicrosericeGarage/MicrosericeGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Renault.MicrosericeGarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosericeGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
