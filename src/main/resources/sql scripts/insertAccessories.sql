625ca92d18f986a0e9a9e3cb30c7231f8d815a7b
Driss ABSIKE <absikedriss25@gmail.com>
2025-11-07 00:44:29 +0100

    add INSERT accessories data

---
+++ src/main/resources/sql scripts/insertAccessories.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.accessories (name, description, price, type, vehicle_id)
+VALUES
+('GPS Garmin', 'Système GPS intégré pour la navigation', 120.00, 'Electronique', 1),
+('Tapis de sol', 'Tapis de sol résistants à l''eau', 40.00, 'Intérieur', 1),
+('Caméra de recul', 'Caméra arrière haute résolution', 180.00, 'Sécurité', 2),
+('Support smartphone', 'Support magnétique pour téléphone', 15.00, 'Accessoire', 3),
+('Couvre-volant cuir', 'Couvre-volant en cuir premium', 25.00, 'Confort', 3);
