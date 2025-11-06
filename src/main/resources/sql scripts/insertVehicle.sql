0a7eb8ffbea424c5abc94576a5f315d417f2a459
Driss ABSIKE <absikedriss25@gmail.com>
2025-11-07 00:37:03 +0100

    add INSERT Vehicle data

---
+++ src/main/resources/sql scripts/insertVehicle.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.vehicles (brand, fabrication_year, fuel_type, garage_id)
+VALUES
+('Renault Clio', 2019, 'Essence', 1),
+('Renault Megane', 2021, 'Diesel', 1),
+('Renault Captur', 2020, 'Hybride', 2),
+('Renault Talisman', 2018, 'Diesel', 2),
+('Renault Zoe', 2022, 'Électrique', 3);
